c93668bd4fc4c520faca544e98df25408362143b
senzxita <rita@strataconsulting.com>
2025-07-17 18:02:43 -0700

    Save progress before pulling

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
-
+import dns from 'dns';
 import userRouter from "./routes/userRoute.js"
 import taskRouter from "./routes/taskRoute.js"
 import forgotPasswordRouter from "./routes/forgotPassword.js"
 
 //app config
-dotenv.config()
+dotenv.config();
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+console.log('Loaded MONGO_URI:', process.env.MONGO_URI);
+
 const app = express()
 const port = process.env.PORT || 8001
 mongoose.set('strictQuery', true);
